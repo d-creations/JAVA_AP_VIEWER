957ced2f2907f96174c45daeda3c5369545371e5
rothdam1 <rothdam1@students.zhaw.ch>
2022-07-27 17:43:19 +0200

    move  with the circal

--- app/src/main/java/ch/dcreations/apviewer/Step3DModel/StepShapes/Curve/SurfaceCurve.java
+++ app/src/main/java/ch/dcreations/apviewer/Step3DModel/StepShapes/Curve/SurfaceCurve.java
@@ -2,10 +2,13 @@ package ch.dcreations.apviewer.Step3DModel.StepShapes.Curve;
 
 import ch.dcreations.apviewer.Step3DModel.StepShapes.*;
 import ch.dcreations.apviewer.Step3DModel.StepShapes.Curve.Conic.Circle;
+import ch.dcreations.apviewer.Step3DModel.StepShapes.Point.CartasianAxisE;
 import ch.dcreations.apviewer.Step3DModel.StepShapes.Point.CartesianPoint;
+import ch.dcreations.apviewer.Step3DModel.StepShapes.Point.Point;
 import ch.dcreations.apviewer.Step3DModel.StepShapes.Vertex.VertexPoint;
 import javafx.scene.control.TreeItem;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -19,6 +22,10 @@ public class SurfaceCurve extends Curve {
         this.curve = curve;
         this.items = items;
         this.representation = representation;
+    }
+
+    public SurfaceCurve(String name, Curve curve, Set<StepShapes> items, PreferredSurfaceCurveRepresentation representation, int lineNumber) {
+        this(name, curve, items, representation, AP242Code.SURFACE_CURVE, lineNumber);
     }
 
     @Override
@@ -37,16 +44,48 @@ public class SurfaceCurve extends Curve {
     }
     private void calculateCurveGeometrie(VertexPoint start, VertexPoint end){
         System.out.println("circle geometrie");
-        Circle circle = (Circle) curve;
-        Axis2Placement3D position =  circle.getPosition();
-        CartesianPoint middlePoint = position.getLocation();
-        Direction axis = position.getAxis();
-        this.edgeDrawingPoints.add(new IncrementalPointsD(0,0,0));
+        try {
+            Circle circle = (Circle) curve;
+            Axis2Placement3D position = circle.getPosition();
+            CartesianPoint middlePoint = position.getLocation();
+            Direction axis = position.getAxis();
+            Direction firstDirectionE = position.getDirection();
+            // Kreuzprodukt
+            List<Double> secondDirectionList = new ArrayList<>();
+            secondDirectionList.add(axis.getDirectionRatios().get(1) * firstDirectionE.getDirectionRatios().get(2) - axis.getDirectionRatios().get(2) * firstDirectionE.getDirectionRatios().get(1)); // add X
+            secondDirectionList.add(axis.getDirectionRatios().get(2) * firstDirectionE.getDirectionRatios().get(0) - axis.getDirectionRatios().get(0) * firstDirectionE.getDirectionRatios().get(2)); // add Y
+            secondDirectionList.add(axis.getDirectionRatios().get(0) * firstDirectionE.getDirectionRatios().get(1) - axis.getDirectionRatios().get(1) * firstDirectionE.getDirectionRatios().get(0)); // add Z
+            Direction secondDirectionE = new Direction("", secondDirectionList, 0);
+
+            //Tranlatation Start Point And Endpoint to 0,0,0 Middlepoint
+            double startXBasisN = 0, startYBasisN = 0, startZBasisN = 0;
+            double endXBasisN = 0, endYBasisN = 0, endZBasisN = 0;
+            startXBasisN = ((CartesianPoint) start.ifExistGivePoint().getPoint()).getPoint().get(CartasianAxisE.X) - middlePoint.getPoint().get(CartasianAxisE.X);
+            startYBasisN = ((CartesianPoint) start.ifExistGivePoint().getPoint()).getPoint().get(CartasianAxisE.Y) - middlePoint.getPoint().get(CartasianAxisE.Y);
+            startZBasisN = ((CartesianPoint) start.ifExistGivePoint().getPoint()).getPoint().get(CartasianAxisE.Z) - middlePoint.getPoint().get(CartasianAxisE.Z);
+            endXBasisN = ((CartesianPoint) end.ifExistGivePoint().getPoint()).getPoint().get(CartasianAxisE.X) - middlePoint.getPoint().get(CartasianAxisE.X);
+            endYBasisN = ((CartesianPoint) end.ifExistGivePoint().getPoint()).getPoint().get(CartasianAxisE.Y) - middlePoint.getPoint().get(CartasianAxisE.Y);
+            endZBasisN = ((CartesianPoint) end.ifExistGivePoint().getPoint()).getPoint().get(CartasianAxisE.Z) - middlePoint.getPoint().get(CartasianAxisE.Z);
+            // move in first dirextion = x   second Direceten = y  trith direction = z   as symbol e
+            double x = 0, y = 0, z = 0;
+
+
+            //Basiswechsel mit dem Einheits Normalvektor
+            double xBasisN = x * firstDirectionE.getDirectionRatios().get(0) + y * secondDirectionE.getDirectionRatios().get(0) + z * axis.getDirectionRatios().get(0);
+            double yBasisN = x * firstDirectionE.getDirectionRatios().get(1) + y * secondDirectionE.getDirectionRatios().get(1) + z * axis.getDirectionRatios().get(1);
+            ;
+            double zBasisN = x * firstDirectionE.getDirectionRatios().get(2) + y * secondDirectionE.getDirectionRatios().get(2) + z * axis.getDirectionRatios().get(2);
+            ;
+            // Transformation to MiddlePoint
+            xBasisN += middlePoint.getPoint().get(CartasianAxisE.X);
+            yBasisN += middlePoint.getPoint().get(CartasianAxisE.Y);
+            zBasisN += middlePoint.getPoint().get(CartasianAxisE.Z);
+            this.edgeDrawingPoints.add(new IncrementalPointsD(xBasisN, yBasisN, zBasisN));
+        }catch (Exception e){
+            System.out.println("Points where not defined ");
+        }
     }
 
-    public SurfaceCurve(String name, Curve curve, Set<StepShapes> items, PreferredSurfaceCurveRepresentation representation, int lineNumber) {
-        this(name, curve, items, representation, AP242Code.SURFACE_CURVE, lineNumber);
-    }
     @Override
     public TreeItem<StepShapes> getTreeItem() {
         TreeItem<StepShapes> treeItem = new TreeItem<>(this);
