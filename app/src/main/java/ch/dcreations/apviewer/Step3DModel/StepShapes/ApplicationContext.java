a314b0d453982e7577d431788d25ff330dd6b691
rothdam1 <rothdam1@students.zhaw.ch>
2022-08-01 12:13:13 +0200

    reformate code

--- app/src/main/java/ch/dcreations/apviewer/Step3DModel/StepShapes/ApplicationContext.java
+++
@@ -1,9 +0,0 @@
-package ch.dcreations.apviewer.Step3DModel.StepShapes;
-
-
-public class ApplicationContext extends StepShapes {
-
-    public ApplicationContext(String name,int lineNumber) {
-        super(AP242Code.APPLICATION_CONTEXT,name,lineNumber);
-    }
-}
